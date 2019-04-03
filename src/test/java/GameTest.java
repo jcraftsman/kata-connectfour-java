bffac96ee7e58b523a6cf29bec81362662508028
Wassel Alazhar <wassel.alazhar@gmail.com>
2019-04-03 16:30:44 +0200

    initial commit

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameTest {
+
+    @Test
+    void name_should() {
+        // Given
+
+        // When
+
+        // Then
+        assertThat(false).isTrue();
+    }
+}
